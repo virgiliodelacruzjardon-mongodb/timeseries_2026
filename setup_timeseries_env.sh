30ae4532669c24843d6b8911d44906a3305e208d
Virgilio De la Cruz <virgilio.delacruzjardon@mongodb.com>
2026-05-19 14:31:50 -0600

    change shell script

--- setup_timeseries_env.sh
+++ setup_timeseries_env.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-python3 -m venv venv
-source venv/bin/activate
-python3 -m pip install --upgrade pip
-python3 -m pip install pymongo
+pip install --upgrade pip
+pip install pymongo
 
 export MONGODB_URI='mongodb+srv://admin:<db_password>@<atlas_cluster_url>/?appName=<cluster_name>'
 
 echo "Virtual environment activated."
 echo "pymongo installed."
 echo "MONGODB_URI configured."
+ 
